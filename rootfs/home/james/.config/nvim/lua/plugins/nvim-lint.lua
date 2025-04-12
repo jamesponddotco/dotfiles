5f5fe8af40bb4162553a9f437b3c8dc0c8a844ad
James Pond <james@cipher.host>
2025-04-12 14:27:36 -0300

    [nvim] Add stylelint to nvim-lint
    Signed-off-by: James Pond <james@cipher.host>

--- rootfs/home/james/.config/nvim/lua/plugins/nvim-lint.lua
+++ rootfs/home/james/.config/nvim/lua/plugins/nvim-lint.lua
@@ -2,9 +2,11 @@ return {
   "mfussenegger/nvim-lint",
   config = function()
     local lint = require("lint")
+    local stylelint = lint.linters.stylelint
 
     lint.linters_by_ft = {
       css = { 'stylelint' },
+      scss = { 'stylelint' },
       javascript = { 'eslint' },
       typescript = { 'eslint' },
       php = {
@@ -12,6 +14,14 @@ return {
         'phpstan',
       },
       sh = { 'shellcheck' },
+    }
+
+    stylelint.args = {
+      '-f',
+      'json',
+      '--quiet-deprecation-warnings',
+      '--stdin',
+      '--stdin-filename'
     }
 
     vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost" }, {
